3a704bb19783ac1af8381bfddde35b0ce19cd819
totsucom <maeoka8bit@gmail.com>
2019-10-08 08:06:02 +0900

    Lチカ

--- Main/Source/main.c
+++ Main/Source/main.c
@@ -1,11 +1,32 @@
+/*
+ * basicio.h でLチカ
+ */
+
 #include "basicio.h"
 
+#define LED 16
+
+// 起動時や起床時に呼ばれる関数
 void setup(bool_t warmWake, uint32_t bitmapWakeStatus) {
+    dio_pinMode(LED, OUTPUT);
 }
 
+// setup()後、３種類のイベントで呼ばれるループ関数
 void loop(EVENTS event) {
+    static bool_t b = TRUE;
+
     if (event == EVENT_START_UP) {
+        // 最初に呼ばれる
+
     } else if (event == EVENT_TICK_SECOND) {
+        // 1秒毎に呼ばれる
+
+        // 1秒毎に点滅を繰り返す
+        // モノスティックの場合はLOW(FALSE)で点灯
+        dio_write(LED, b);
+        b = !b;
     } else if (event == EVENT_TICK_TIMER) {
+        // 4ミリ秒毎(デフォルト)に呼ばれる
+
     }
 }
